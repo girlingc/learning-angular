b164ef389eb060345bcce8dd3f7fbfdc63c99c50
Christian Girling <girling.christian@gmail.com>
2022-04-15 11:03:12 -0700

    Display child message from parent

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
+  @Input() childMessage!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
